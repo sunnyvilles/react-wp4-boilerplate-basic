fa48f9350113a36630694edd229577a97aaaae15
sunnyvilles <sunnyvilles@himanshus-MacBook-Air.local>
2019-12-08 19:20:29 +0100

    self setup basic

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const htmlWP = require('html-webpack-plugin');
+
+module.exports({
+	entry:'./src',
+	output: {
+		path: path.join(__dirname + '/dist'),
+		filename : 'bundle.js'
+	},
+	//loaders go inside modules obj rules array , which is an array of objects
+	//objects take test key which are regex
+	module:{
+		rules:[
+			{
+				test:/\.js$/, //looks at jsx also
+				exclude: 'node_modules',
+				use:{
+					loader: 'babel-loader'
+				}
+			}
+		]
+	}
+})
