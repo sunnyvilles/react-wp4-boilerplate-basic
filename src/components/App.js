2d4776ac5c28bdc076389ba8cf32b9daf89559fc
sunnyvilles <sunny.era@gmail.com>
2020-09-06 21:16:18 +0000

    reformat

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 function App() {
 
-		return <div>
-					<h1>Speed typing Game</h1>
-				</div>
+    return <div>
+                <h1>Speed typing Game</h1>
+            </div>
 	
 }
 
