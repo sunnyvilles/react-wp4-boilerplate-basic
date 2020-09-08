db907e71d51a59521b6f369a07d3633e28bdba81
sunnyvilles <sunny.era@gmail.com>
2020-09-08 20:18:45 +0000

    css,style loader webpack with some styling

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,12 @@ module.exports = {
 				use:{
 					loader: 'babel-loader'
 				}
-			}
+            },
+            {
+                test:/\.css$/,
+                exclude: /node_modules/,
+                use:["style-loader","css-loader"]
+            }
 		]
 	},
 	plugins:[
